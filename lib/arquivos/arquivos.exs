35461f44dd04ea202cb2aaea0cacddbf39c20fb2
Danilo Kodavara <danilo.kodavara@hotmail.com>
2022-02-12 20:39:05 -0300

    Criando metodo de leitura de arquivo, tratando erro de forma generica.

--- lib/arquivos/arquivos.exs
+++ lib/arquivos/arquivos.exs
@@ -2,4 +2,12 @@ defmodule Teste.Arquivos do
   def ler(arquivo) do
     File.read!(arquivo)
   end
+
+  def ler2(arquivo) do
+    case File.read(arquivo) do
+      {:ok, conteudo} -> IO.puts(conteudo)
+      {:error,_} -> "Aconteceu algum erro"
+    end
+  end
+
 end
