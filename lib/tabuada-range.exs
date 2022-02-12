0a4f3208f191742e03da486f72c73bc84f519461
Danilo Kodavara <danilo.kodavara@hotmail.com>
2022-02-12 16:51:18 -0300

    Utilizando pipe para funcao de soma.

--- lib/tabuada-range.exs
+++ lib/tabuada-range.exs
@@ -1,7 +1,18 @@
 defmodule Teste.Tabuada do
 
+  require Integer
+
   def calcula(multiplo) when (is_integer(multiplo)) do
     Enum.map(1..10, &(&1 * multiplo))
+  end
+
+  def calculaPar(multiplo) when (is_integer(multiplo)) do
+    tabuadaList = Enum.map(1..10, &(&1 * multiplo))
+    Enum.filter(tabuadaList, &Integer.is_even(&1))
+  end
+
+  def calculaSoma(multiplo) when (is_integer(multiplo)) do
+    1..10 |> Enum.map(&(&1 * multiplo)) |> Enum.filter(&Integer.is_even(&1)) |> Enum.sum()
   end
 
 end
