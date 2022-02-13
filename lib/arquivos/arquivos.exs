a711d3ef341d0f18263b254d46aa81b2cff363fa
Danilo Kodavara <danilo.kodavara@hotmail.com>
2022-02-12 21:45:19 -0300

    utilizando pattern matching para lancar os erros referente a abertura e escrita nos arquivos.

--- lib/arquivos/arquivos.exs
+++ lib/arquivos/arquivos.exs
@@ -1,16 +1,16 @@
 defmodule Teste.Arquivos do
-  def ler(arquivo) do
+  def simple_read(arquivo) do
     File.read!(arquivo)
   end
 
-  def ler2(arquivo) do
+  def generic_read(arquivo) do
     case File.read(arquivo) do
       {:ok, conteudo} -> IO.puts(conteudo)
       {:error,_} -> "Aconteceu algum erro"
     end
   end
 
-  def ler3(arquivo) do
+  def specific_read(arquivo) do
     case File.read(arquivo) do
       {:ok, conteudo} -> IO.puts(conteudo)
       {:error, :enoent} -> "Arquivo inexistente"
@@ -18,5 +18,28 @@ defmodule Teste.Arquivos do
       {:error,_} -> "Erro desconhecido"
     end
   end
+
+  def write(arquivo) do
+    File.write(arquivo, "Conteudo do arquivo")
+  end
+
+  def open(file_path) do
+    case File.open(file_path, [:write, :utf8]) do
+      {:ok, file} -> write_with_io(file)
+      {:error,_} -> "Erro ao tentar abrir arquivo."
+    end
+  end
+
+  defp write_with_io(file) do
+    case IO.write(file, "Escrevendo pela terceira vez") do
+      :ok -> close_file(file)
+      :error -> "Erro ao escrever arquivo"
+    end
+  end
+
+  defp close_file(file) do
+    File.close(file)
+  end
+
 
 end
